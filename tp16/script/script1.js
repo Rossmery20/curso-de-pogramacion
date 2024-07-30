1b1a77835a32637bdddbf3a935f15fc78498dc45
unknown <mery_rossy05@hotmail.com>
2024-07-30 17:18:24 -0300

    inicio de javascript

---
+++ tp16/script/script1.js
@@ -0,0 +1,11 @@
+console.log("soy un log");
+console.info("soy un loro");
+console.warn("soy un warn");
+console.error("soy un error");
+
+console.table({
+    nombre:"ross",
+    apellido:"vila",
+    dni:94492375,
+})
+window.prompt("ingrese un dato");
---
+++ tp16/script/scrpt2.js
@@ -0,0 +1,38 @@
+let nombre;
+nombre ="ross";
+nombre ="mery";
+console.log(nombre);
+
+const apellido ="vila";
+//apellido="callizaya";
+console.log(apellido);
+
+//let nombrecompleto =nombre + " " + apellido;
+//console.log(nombrecompleto);
+
+let dni =window.prompt("ingrese su numero de dni");
+console.log("su DNI es " +dni);
+
+let numero1 = 6;
+let numero2 = 4;
+let suma =numero1 + numero2;
+console.log(suma);
+
+/* let numero3 = window.prompt("ingrese el primer numero");
+numero3 = Number(numero3);
+let numero4 = window.prompt("ingrese el segundo numero");
+numero4 =Number(numero4);
+let sumadeimputs =numero3 +numero4;
+console.log(sumadeimputs); */
+
+//scoope 
+let num = 12;
+console.log(num)
+{
+    let num =13;
+    console.log(num);
+    let letra ="A";
+    console.log (letra);
+}
+console.log(num);
+console.log(letra)
